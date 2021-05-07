545832db6896c930366b116ddf119098b8c5deb5
Mayoor Rao <mayoor.rao@oracle.com>
2021-05-06 21:04:37 -0700

    added training step

--- exp.py
+++ exp.py
@@ -1,5 +1,28 @@
+import sklearn
+import pandas as pd
+import joblib
+from sklearn.datasets import load_boston
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+import os
+
+
 def train(logger, **kwargs):
-    logger.log("I am training")
+    boston_data = load_boston()
+    df = pd.DataFrame(boston_data.data, columns=boston_data.feature_names)
+    target = boston_data.target
+    rf_model = RandomForestRegressor(n_jobs=-1, max_depth=5, random_state=42)
+    xtrain, xtest, ytrain, ytest = train_test_split(
+        df, target, test_size=0.2, random_state=42
+    )
+    rf_model.fit(xtrain, ytrain)
+    logger.log(f"Test score is: {r2_score(ytest, rf_model.predict(xtest))}")
+    output_dir = os.environ["OUTPU_DIR"]
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    with open(os.path.join(output_dir, "model.jlib"), "wb") as mf:
+        joblib.dump(rf_model, mf)
 
 
 def test(logger, **kwargs):
